16a15d35d8dfe145474fdd3ba5319f515d4a5423
KLIMANN INES <klimann@informatique.univ-paris-diderot.fr>
2021-06-08 14:15:53 +0200

    script pour l'affichage du plan

---
+++ Plan/README.md
@@ -0,0 +1,2 @@
+* Un répertoire par matière.
+* Un fichier par date de nom aaaammjj. Sur les premières lignes un résumé pour la synthèse, puis une ligne de séparation avec #, puis plus de détails.
---
+++ Plan/webpage.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import os
+import locale
+from datetime import datetime
+
+topics = { 'algorithmic':{'dir': 'Algorithmique', 'md':'algorithmique.md', 'title':'Algorithmique'},
+           'databases':{'dir':'BasesDeDonnees','md':'bd.md', 'title':'Bases de données'},
+           'coding':{'dir':'Programmation','md':'prog.md', 'title':'Programmation'},
+           'systems':{'dir':'Systemes','md':'systemes.md','title':'Systèmes'} }
+synthesis = 'synthese.md'
+
+
+class Course :
+    def __init__(self, dir, file):
+        self.part = dir
+        self.date = file
+        self.set_data(dir, file)
+
+    def set_data(self, dir, file):
+        f = open(dir + '/' + file, 'r')
+        s = ""
+        for line in f:
+            if line[0] == '#':
+                self.abstract = s
+                s = ""
+                continue
+            s += line
+        self.description = s
+
+
+def not_yet(file) :
+    return file[0] == '_'
+
+def headers():
+    with open(synthesis, 'w') as f :
+        f.write("""##Plan du cours\n
+|   | """)
+        for topic in sorted(topics.values(), key=lambda t: t['title']):
+            f.write(topic['title'] + ' | ')
+        f.write("""\n|---|-----------|---------|---------|-------|\n""")
+        f.close()
+    for topic in topics.values() :
+        with open(topic['md'], 'w') as f :
+            f.write('##' + topic['title'] + '\n\n')
+            f.close()
+
+
+def nice_date(date):
+    return datetime.strptime(date, "%Y%m%d").strftime("%A %-d %B %Y")
+            
+def write_course(c, f):
+    f.write('* ' + nice_date(c.date) + ' : ')
+    f.write(c.abstract)
+    f.write(c.description)
+
+def write_in_right_position(c, f) :
+    ordered_topics = sorted([ t['title'] for t in topics.values()])
+    n = ordered_topics.index(c.part)
+    for i in range(0, len(ordered_topics)) :
+        if i==n : f.write(c.abstract.strip().replace('\n', '<br />'))
+        f.write(' | ')
+    f.write('\n')
+            
+def bodies(courses):
+    for topic in topics.values():
+        #get only the courses for this topic
+        topic_courses = list(filter(lambda c: c.part == topic['dir'], courses))
+        topic_courses.sort(key=lambda c: c.date) # in chronological order
+        with open(topic['md'], 'a') as f :
+            for c in topic_courses :
+                write_course(c, f)
+            f.close()
+    courses.sort(key=lambda c: c.date)
+    with open(synthesis, 'a') as f:
+        for c in courses:
+            f.write('| '+ nice_date(c.date) + ' | ')
+            write_in_right_position(c, f)
+        f.close()
+
+
+def write_foot(f):
+    f.write("\n\n\n")
+    f.write("[Synthèse](synthese.md) ")
+    for topic in topics.values() :
+        f.write('[' + topic['title'] + '](' + topic['md'] + ') ')
+    f.write('\n\n\n')
+        
+        
+def footers():
+    with open(synthesis, 'a') as f :
+        write_foot(f)
+        f.close()
+    for topic in topics.values() :
+        with open(topic['md'], 'a') as f :
+            write_foot(f)
+            f.close()
+        
+            
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_TIME,'')
+    headers()
+    courses = []
+    for topic in topics.values() :
+        dir = topic['dir']
+        for file in os.listdir(dir) :
+            if not_yet(file) : continue
+            print("adding " + dir + '/' + file)
+            courses.append(Course(dir, file))
+    bodies(courses)
+    footers()
