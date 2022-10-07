41e23b6e856897ccff6d883b888eb71d695c77fb
KLIMANN INES <klimann@zaclys.net>
2022-10-07 15:21:50 +0200

    typo

--- docs/TP2022-2023/TP_tableaux_unidimensionnels/tests/complement_a_deux.c
+++ docs/TP2022-2023/TP_tableaux_unidimensionnels/tests/complement_a_deux.c
@@ -14,17 +14,17 @@ bool assert_identiques(bool *r, bool *rep){
 int main(void){
   bool *r0 = complement_a_deux(0);
   bool rep0[8] = {0,0,0,0,0,0,0,0};
-  bool *r1 = complement_a_deux(0);
+  bool *r1 = complement_a_deux(1);
   bool rep1[8] = {0,0,0,0,0,0,0,1};
-  bool *rm1 = complement_a_deux(0);
+  bool *rm1 = complement_a_deux(-1);
   bool repm1[8] = {1,1,1,1,1,1,1,1};
-  bool *r4 = complement_a_deux(0);
+  bool *r4 = complement_a_deux(4);
   bool rep4[8] = {0,0,0,0,0,1,0,0};
-  bool *rm4 = complement_a_deux(0);
+  bool *rm4 = complement_a_deux(-4);
   bool repm4[8] = {1,1,1,1,1,1,0,0};
-  bool *r10 = complement_a_deux(0);
+  bool *r10 = complement_a_deux(10);
   bool rep10[8] = {0,0,0,0,1,0,1,0};
-  bool *rm10 = complement_a_deux(0);
+  bool *rm10 = complement_a_deux(-10);
   bool repm10[8] = {1,1,1,1,0,1,1,0};
 
   assert(assert_identiques(r0, rep0));
