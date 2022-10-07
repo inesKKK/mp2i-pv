8804243f3ce41718195a8a2e73ad997e5379326e
KLIMANN INES <klimann@zaclys.net>
2022-10-07 11:53:17 +0200

    typo

--- docs/TP2022-2023/TP_tableaux_unidimensionnels/tests/complement_a_deux.c
+++ docs/TP2022-2023/TP_tableaux_unidimensionnels/tests/complement_a_deux.c
@@ -15,25 +15,25 @@ int main(void){
   bool *r0 = complement_a_deux(0);
   bool rep0[8] = {0,0,0,0,0,0,0,0};
   bool *r1 = complement_a_deux(0);
-  bool rep1[8] = {0,0,0,0,0,0,0,0};
+  bool rep1[8] = {0,0,0,0,0,0,0,1};
   bool *rm1 = complement_a_deux(0);
-  bool repm1[8] = {0,0,0,0,0,0,0,0};
+  bool repm1[8] = {1,1,1,1,1,1,1,1};
   bool *r4 = complement_a_deux(0);
-  bool rep4[8] = {0,0,0,0,0,0,0,0};
+  bool rep4[8] = {0,0,0,0,0,1,0,0};
   bool *rm4 = complement_a_deux(0);
-  bool repm4[8] = {0,0,0,0,0,0,0,0};
+  bool repm4[8] = {1,1,1,1,1,1,0,0};
   bool *r10 = complement_a_deux(0);
-  bool rep10[8] = {0,0,0,0,0,0,0,0};
+  bool rep10[8] = {0,0,0,0,1,0,1,0};
   bool *rm10 = complement_a_deux(0);
-  bool repm10[8] = {0,0,0,0,0,0,0,0};
+  bool repm10[8] = {1,1,1,1,0,1,1,0};
 
-  assert_identiques(r0, rep0);
-  assert_identiques(r1, rep1);
-  assert_identiques(rm1, repm1);
-  assert_identiques(r4, rep4);
-  assert_identiques(rm4, repm4);
-  assert_identiques(r10, rep10);
-  assert_identiques(rm10, repm10);
+  assert(assert_identiques(r0, rep0));
+  assert(assert_identiques(r1, rep1));
+  assert(assert_identiques(rm1, repm1));
+  assert(assert_identiques(r4, rep4));
+  assert(assert_identiques(rm4, repm4));
+  assert(assert_identiques(r10, rep10));
+  assert(assert_identiques(rm10, repm10));
   
   free(r0);
   free(r1);
