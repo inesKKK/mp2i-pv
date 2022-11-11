d73cc78d449c9d101efb6a4114d0bfe456b3ce54
Alexis Rossfelder <contact.liteapplication@gmail.com>
2022-11-11 11:17:25 +0100

    format python

--- docs/Cours/code/fibonacci.py
+++ docs/Cours/code/fibonacci.py
@@ -1,6 +1,8 @@
 # exemple en python
-def fibonacci (n : int) -> int :
-    if n==0 : return 0
-    elif n==1 : return 1
-    else :
-        return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
