d2dbcf1c4c9410c041ef201c2a0b0da57e6200a1
KLIMANN INES <klimann@zaclys.net>
2024-09-16 22:48:32 +0200

    definition de xtest

--- docs/TP2024-2025/TP02_premieres_fonctions/tests/test_racine_Heron.c
+++ docs/TP2024-2025/TP02_premieres_fonctions/tests/test_racine_Heron.c
@@ -4,7 +4,8 @@
 int main(void){
   double epsilon = 0.001;
   double precisiontest = 0.0000001;
-
+  double xtest = 43643285.43;
+  
   assert(fabs(racine_Heron(0.1, precisiontest) - sqrt(.1)) < epsilon);
   assert(fabs(racine_Heron(.5, precisiontest) - sqrt(.5)) < epsilon);
   assert(fabs(racine_Heron(xtest, precisiontest) - sqrt(xtest)) < epsilon);
