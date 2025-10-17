ca890e1a7fea0e3215701342a750e94152342208
inesKKK <50366737+inesKKK@users.noreply.github.com>
2025-10-17 10:41:02 +0200

    Add files via upload

--- docs/TP2025-2026/TP07_compilation_sur_plusieurs_fichiers/tests/tests_mandelbrot.c
+++ docs/TP2025-2026/TP07_compilation_sur_plusieurs_fichiers/tests/tests_mandelbrot.c
@@ -5,29 +5,29 @@
 #include "tests_mandelbrot.h"
 
 void test_point_suivant(){
-  /*
-  double x, y;
+  struct point p;
+  struct point origine = {.x=0, .y=0}, p1 = {.x=1, .y=1}, c1 = {.x=2, .y=3},
+    p2 = {.x=1.5, .y=-2.3}, c2 = {.x=-2, .y=.5};
   double epsilon = 0.00001;
 
-  point_suivant(0, 0, 0, 0, &x, &y);
-  assert(fabs(x)<epsilon && fabs(y)<epsilon);
+  point_suivant(origine, origine, &p);
+  assert(fabs(p.x)<epsilon && fabs(p.y)<epsilon);
 
-  point_suivant(1, 1, 2, 3, &x, &y);
-  assert(fabs(x-2)<epsilon && fabs(y-5)<epsilon);
+  point_suivant(p1, c1, &p);
+  assert(fabs(p.x-2)<epsilon && fabs(p.y-5)<epsilon);
 
-  point_suivant(1.5, -2.3, -2, .5, &x, &y);
-  assert(fabs(x+5.04)<epsilon && fabs(y+6.4)<epsilon);
-  */
+  point_suivant(p2, c2, &p);
+  assert(fabs(p.x+5.04)<epsilon && fabs(p.y+6.4)<epsilon);
 }
 
 
 void test_suite_bornee(){
-  /*
-  assert(suite_bornee(0,0));
-  assert(!suite_bornee(1,1));
-  assert(suite_bornee(.01,.01));
-  assert(!suite_bornee(1,0));
-  */
+  struct point p1 = {.x=0, .y=0}, p2 = {.x=1, .y=1},
+    p3 = {.x=.01, .y=.01}, p4 = {.x=1, .y=0};
+  assert(suite_bornee(p1));
+  assert(!suite_bornee(p2));
+  assert(suite_bornee(p3));
+  assert(!suite_bornee(p4));
 }
 
 int main(void){
